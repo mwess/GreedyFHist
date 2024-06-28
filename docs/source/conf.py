4a6d316bc55f26bc3190f8c32065a9e740147615
Maxi <maximilian.wess@ntnu.no>
2024-06-29 01:38:11 +0200

    added template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sphinx_rtd_theme
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -28,5 +31,45 @@ master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+
+
+
+# -- Options for HTML output
+
+html_theme = "sphinx_rtd_theme"
+html_context = {
+    "sidebar_external_links_caption": "Links",
+    "sidebar_external_links": [
+        (
+            '<i class="fa fa-rss fa-fw"></i> Blog',
+            "https://www.poliastro.space",
+        ),
+        (
+            '<i class="fa fa-github fa-fw"></i> Source code',
+            "https://github.com/poliastro/poliastro",
+        ),
+        (
+            '<i class="fa fa-bug fa-fw"></i> Issue tracker',
+            "https://github.com/poliastro/poliastro/issues",
+        ),
+        (
+            '<i class="fa fa-envelope fa-fw"></i> Mailing list',
+            "https://groups.io/g/poliastro-dev",
+        ),
+        (
+            '<i class="fa fa-comments fa-fw"></i> Chat',
+            "http://chat.poliastro.space",
+        ),
+        (
+            '<i class="fa fa-file-text fa-fw"></i> Citation',
+            "https://doi.org/10.5281/zenodo.593610",
+        ),
+    ],
+}
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_style = os.path.join("css", "custom.css")
+html_favicon = os.path.join("_static", "favicon.ico")
+html_static_path = ["_static"]
+html_js_files = ["language_data.js"]
