8698aac361fb34f866c394f76296e680a8c2580c
Maxi <maximilian.wess@ntnu.no>
2024-06-29 02:02:53 +0200

    1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,35 +39,35 @@ master_doc = 'index'
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
-html_context = {
-    "sidebar_external_links_caption": "Links",
-    "sidebar_external_links": [
-        (
-            '<i class="fa fa-rss fa-fw"></i> Blog',
-            "https://www.poliastro.space",
-        ),
-        (
-            '<i class="fa fa-github fa-fw"></i> Source code',
-            "https://github.com/poliastro/poliastro",
-        ),
-        (
-            '<i class="fa fa-bug fa-fw"></i> Issue tracker',
-            "https://github.com/poliastro/poliastro/issues",
-        ),
-        (
-            '<i class="fa fa-envelope fa-fw"></i> Mailing list',
-            "https://groups.io/g/poliastro-dev",
-        ),
-        (
-            '<i class="fa fa-comments fa-fw"></i> Chat',
-            "http://chat.poliastro.space",
-        ),
-        (
-            '<i class="fa fa-file-text fa-fw"></i> Citation',
-            "https://doi.org/10.5281/zenodo.593610",
-        ),
-    ],
-}
+# html_context = {
+#     "sidebar_external_links_caption": "Links",
+#     "sidebar_external_links": [
+#         (
+#             '<i class="fa fa-rss fa-fw"></i> Blog',
+#             "https://www.poliastro.space",
+#         ),
+#         (
+#             '<i class="fa fa-github fa-fw"></i> Source code',
+#             "https://github.com/poliastro/poliastro",
+#         ),
+#         (
+#             '<i class="fa fa-bug fa-fw"></i> Issue tracker',
+#             "https://github.com/poliastro/poliastro/issues",
+#         ),
+#         (
+#             '<i class="fa fa-envelope fa-fw"></i> Mailing list',
+#             "https://groups.io/g/poliastro-dev",
+#         ),
+#         (
+#             '<i class="fa fa-comments fa-fw"></i> Chat',
+#             "http://chat.poliastro.space",
+#         ),
+#         (
+#             '<i class="fa fa-file-text fa-fw"></i> Citation',
+#             "https://doi.org/10.5281/zenodo.593610",
+#         ),
+#     ],
+# }
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_style = os.path.join("css", "custom.css")
 # html_favicon = os.path.join("_static", "favicon.ico")
@@ -83,8 +83,13 @@ html_js_files = ["language_data.js"]
 #     ]
 # }
 
+# html_sidebars = {
+#     '**': [
+#         'globaltoc.html'
+#     ]
+# }
+
 html_sidebars = {
-    '**': [
-        'globaltoc.html'
-    ]
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+   'using/windows': ['windowssidebar.html', 'searchbox.html'],
 }
