e2349d0d116cdc9adbf903f69844e53fff157172
Maxi <maximilian.wess@ntnu.no>
2024-07-14 17:06:59 +0200

    missing example data

---
+++ examples/scripts/example_pairwise_registation.sh
@@ -0,0 +1,8 @@
+#python ../../../greedyfhist/__main__.py register \
+greedyfhist register \
+    --moving-image ../pairwise_examples/images/moving_image.ome.tif \
+    --fixed-image ../pairwise_examples/images/fixed_image.ome.tif \
+    --config example_registration.toml \
+    --pointsets ../pairwise_examples/annotations/moving_pointset.csv \
+
+
