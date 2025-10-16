158547485c7a8631bcb530be47b9dfc82dd261c3
mwess <maximilian.wess@ntnu.no>
2025-10-16 16:40:56 +0200

    half reverted decorator

--- greedyfhist/utils/decorators.py
+++ greedyfhist/utils/decorators.py
@@ -6,9 +6,10 @@ from bioio import BioImage
 def reset_scene_after_use(func):
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
-        if not 'img_data' in self.__dict__ and not isinstance(self.__getattribute__('img_data'), BioImage):
+        var_name = 'img_data'
+        if not var_name in self.__dict__ and not isinstance(self.__getattribute__(var_name), BioImage):
             raise AttributeError('Decorator cannot be applied on this object.')
-        img_data: BioImage = self.__getattribute__('img_data')
+        img_data: BioImage = self.__getattribute__(var_name)
         current_scene = img_data.current_scene
         res = func(self, *args, **kwargs)
         img_data.set_scene(current_scene)
